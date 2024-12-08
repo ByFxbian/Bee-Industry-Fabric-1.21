c3f7ae05abd5393115ae1ed74a921eff8cda60fe
ByFxbian <65723620+ByFxbian@users.noreply.github.com>
2024-12-08 22:39:35 +0100

    Initial commit

---
+++ src/main/java/at/byfxbian/beeindustry/BeeIndustryModClient.java
@@ -0,0 +1,10 @@
+package at.byfxbian.beeindustry;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BeeIndustryModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
